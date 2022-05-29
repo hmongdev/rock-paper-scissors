83699c18e4db5c0b67872af42d56de2e79ab6e2a
Avery Yang <averyyang2674@gmail.com>
2022-05-29 13:02:52 -0500

    clicking button displays appropriate id

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log(`hello world`);
+$(document).ready(readyNow);
+
+function readyNow() {
+    //add inputs to rock, paper, scissor buttons
+    $('button').on('click', playerChoice);
+    console.log('Added functions to buttons!');
+}
+
+let choice;
+
+function playerChoice(button) {
+    //decide what button the player chose
+    if (button.target.id === 'rock') {
+        choice = 'rock'
+    } else if (button.target.id === 'paper') {
+        choice = 'paper'
+    } else if (button.target.id === 'scissor') {
+        choice = 'scissor'
+    }
+    //return value of button
+    console.log(`You chose ${choice}!`);
+    //target images
+    $()
+    //push button value into img
+    //display image for human player
+}
