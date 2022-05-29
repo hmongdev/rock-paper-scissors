a74c7653f693e73267067c234f49a7e302b22842
Avery Yang <averyyang2674@gmail.com>
2022-05-29 13:11:35 -0500

    clicking buttons shows appropriate picture!

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ function playerChoice(button) {
     //decide what button the player chose
     if (button.target.id === 'rock') {
         choice = 'rock'
+        $('#user').attr('src', "/images/rock.png");
     } else if (button.target.id === 'paper') {
         choice = 'paper'
+        $('#user').attr('src', "/images/paper.png");
+
     } else if (button.target.id === 'scissor') {
         choice = 'scissor'
+        $('#user').attr('src', "/images/scissor.png");
     }
     //return value of button
     console.log(`You chose ${choice}!`);
     //target images
-    $()
     //push button value into img
     //display image for human player
 }
