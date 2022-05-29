08b28bdd95ccf20bffd68b009dfb9ab2f533c1a7
Avery Yang <averyyang2674@gmail.com>
2022-05-29 13:59:20 -0500

    solved all of logic!

--- script.js
+++ script.js
@@ -2,28 +2,75 @@ $(document).ready(readyNow);
 
 function readyNow() {
     //add inputs to rock, paper, scissor buttons
-    $('button').on('click', playerChoice);
+    $('button').on('click', buttonChoose);
     console.log('Added functions to buttons!');
 }
 
-let choice;
+let playerChoice;
+let computerChoice;
+console.log(`Computer choice is: ${computerChoice}`);
+let buttons = ['rock', 'paper', 'scissor']
 
-function playerChoice(button) {
-    //decide what button the player chose
+function buttonChoose(button) {
+    //if...else that decides what button the player chose
     if (button.target.id === 'rock') {
-        choice = 'rock'
-        $('#user').attr('src', "/images/rock.png");
+        playerChoice = 'rock'
+        //target and change images
+        $('#playerImage').attr('src', "/images/rock.png");
     } else if (button.target.id === 'paper') {
-        choice = 'paper'
-        $('#user').attr('src', "/images/paper.png");
-
+        playerChoice = 'paper'
+        $('#playerImage').attr('src', "/images/paper.png");
     } else if (button.target.id === 'scissor') {
-        choice = 'scissor'
-        $('#user').attr('src', "/images/scissor.png");
+        playerChoice = 'scissor'
+        $('#playerImage').attr('src', "/images/scissor.png");
     }
-    //return value of button
-    console.log(`You chose ${choice}!`);
-    //target images
-    //push button value into img
-    //display image for human player
+    //randomly choose response for computer
+    computerChoose();
+    //determine winner
+    whoWins(playerChoice, computerChoice);
+}
+
+
+function computerChoose() {
+    //choose random index from buttons
+    //~~ operator is Math.floor() so we will return the index 0
+    let randomButtonIndex = ~~(Math.random() * buttons.length);
+    //choose from rock, paper, scissors
+    computerChoice = buttons[randomButtonIndex];
+    console.log(`Computer chooses`, computerChoice);
+    //change computerImage
+    if (computerChoice === 'rock') {
+        $('#computerImage').attr('src', "/images/rock.png");
+    } else if (computerChoice === 'paper') {
+        $('#computerImage').attr('src', "/images/paper.png");
+    } else if (computerChoice === 'scissor') {
+        $('#computerImage').attr('src', "/images/scissor.png");
+    }
+}
+
+function whoWins(player, computer) {
+    //player chooses rock
+    if (player === 'rock' && computer === 'rock') {
+        $('#outcome').text('Tied!').css('color', "yellow");
+    } else if (player === 'rock' && computer === 'paper') {
+        $('#outcome').text('You Lose!').css('color', 'red');
+    } else if (player === 'rock' && computer === 'scissor') {
+        $('#outcome').text('You Win!').css('color', 'green');
+    }
+    //player chooses paper
+    else if (player === 'paper' && computer === 'rock') {
+        $('#outcome').text('You Win!').css('color', 'green');
+    } else if (player === 'paper' && computer === 'paper') {
+        $('#outcome').text('Tied!').css('color', "yellow");
+    } else if (player === 'paper' && computer === 'scissor') {
+        $('#outcome').text('You Lose!').css('color', 'red');
+    }
+    //player chooses scissor
+    else if (player === 'scissor' && computer === 'rock') {
+        $('#outcome').text('You Lose!').css('color', 'red');
+    } else if (player === 'scissor' && computer === 'paper') {
+        $('#outcome').text('You Win!').css('color', 'green');
+    } else if (player === 'scissor' && computer === 'scissor') {
+        $('#outcome').text('Tied!').css('color', "yellow");
+    }
 }
